330958b8e51fc6252114ab413687516dbfd94e87
vvishnu17 <vishnuramadass17@gmail.com>
2024-08-25 20:15:25 +0200

    Add icons on top of video player

--- app/components/VideoExercise.jsx
+++ app/components/VideoExercise.jsx
@@ -3,6 +3,19 @@ import ReactPlayer from "react-player/vimeo";
 
 
 const VideoExercise = ({ exercise }) => {
+  const handleLike = () => {
+    console.log("Liked!");
+    // Add your like functionality here
+  };
+  const handleTime = () => {
+    console.log("Current time: 0 seconds");
+    // Add your Timer functionality here
+  };
+
+  const handleShare = () => {
+    console.log("Shared!");
+    // Add your share functionality here
+  };
   return (
     <>
       <ReactPlayer
@@ -12,6 +25,26 @@ const VideoExercise = ({ exercise }) => {
         height="100%"
         className="videoplayer"
       />
+      <div className="video-icons">
+        <img
+          src="/buttons/like-button.svg"
+          alt="Like"
+          className="video-icon"
+          onClick={handleLike}
+        />
+        <img
+          src="/buttons/time-button.svg"
+          alt="Time"
+          className="video-icon"
+          onClick={handleTime}
+        />
+        <img
+          src="/buttons/share-button.svg"
+          alt="Share"
+          className="video-icon"
+          onClick={handleShare}
+        />
+      </div>
     </>
   );
 };
